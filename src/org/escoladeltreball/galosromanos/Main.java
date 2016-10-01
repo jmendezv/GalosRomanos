cdbf336b089e1c53198d8077e6cd337e5c35b49a
Pep Mendez <jmendez1@xtec.cat>
2016-10-01 14:04:44 +0200

    First commit Galos Romanos

---
+++ src/org/escoladeltreball/galosromanos/Main.java
@@ -0,0 +1,15 @@
+package org.escoladeltreball.galosromanos;
+
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
+public class Main {
+	
+	private static final Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int n = sc.nextInt();
+	}
+
+}
